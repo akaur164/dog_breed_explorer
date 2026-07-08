02f1f09b11fb9bd6ba54c69656f3793b0670ae5c
Arshpreet <akUR154@rrc.ca>
2026-07-08 15:49:07 -0500

    Add model associations

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,10 @@
 class Breed < ApplicationRecord
   belongs_to :group
 end
+class Breed < ApplicationRecord
+  belongs_to :group
+
+  has_many :reviews
+  has_many :breed_temperaments
+  has_many :temperaments, through: :breed_temperaments
+end
--- app/models/temperament.rb
+++ app/models/temperament.rb
@@ -1,2 +1,4 @@
 class Temperament < ApplicationRecord
+  has_many :breed_temperaments
+  has_many :breeds, through: :breed_temperaments
 end
